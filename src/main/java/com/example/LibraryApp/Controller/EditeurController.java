bda6cae3e40ae2bd5b3d422f93ca771d893d0390
@HamzaElabboubi <elabboubihamza8@gmail.com>
2026-04-13 12:39:34 +0200

    editeur service and controller added

--- src/main/java/com/example/LibraryApp/Controller/EditeurController.java
+++ src/main/java/com/example/LibraryApp/Controller/EditeurController.java
@@ -1,4 +1,44 @@
 package com.example.LibraryApp.Controller;
 
+
+import com.example.LibraryApp.DTO.EditeureDTO;
+import com.example.LibraryApp.Repo.EditeurRepo;
+import com.example.LibraryApp.Services.EditeurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class EditeurController {
+
+    @Autowired EditeurService editeurService;
+
+
+    @PostMapping("api/creatEditeur")
+    public EditeureDTO creatEditeur( @RequestBody EditeureDTO dto){
+        return editeurService.createEditeur(dto);
+    }
+
+    @GetMapping("api/allEditeur")
+    public List<EditeureDTO> getAllEditeur(){
+        return  editeurService.getAllEditeur();
+    }
+
+    @GetMapping("api/getEditeur/{id}")
+    public EditeureDTO getEditeur(@PathVariable Long id){
+        return editeurService.getEditeur(id);
+    }
+
+    @PutMapping("api/updateEditeur/{id}")
+    public EditeureDTO updateEditeur(@PathVariable Long id, @RequestBody EditeureDTO dto){
+        return editeurService.updateEditeur(id,dto);
+    }
+
+    @DeleteMapping("api/deleteEditeur/{id}")
+    public String deleteEditeur(@PathVariable Long id){
+        return editeurService.deleteEditeur(id);
+    }
+
 }
+
--- src/main/java/com/example/LibraryApp/Services/EditeurService.java
+++ src/main/java/com/example/LibraryApp/Services/EditeurService.java
@@ -1,7 +1,57 @@
 package com.example.LibraryApp.Services;
 
+import com.example.LibraryApp.DTO.EditeureDTO;
+import com.example.LibraryApp.Mapper.EditeurMapper;
+import com.example.LibraryApp.Model.Editeure;
+import com.example.LibraryApp.Repo.EditeurRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EditeurService {
+
+    @Autowired EditeurRepo editeurRepo;
+    @Autowired EditeurMapper editeurMapper;
+
+    public EditeureDTO createEditeur(EditeureDTO dto){
+        Editeure editeure = editeurMapper.ToEntity(dto);
+        Editeure saved = editeurRepo.save(editeure);
+
+        return editeurMapper.toDTO(saved);
+    }
+
+    public List<EditeureDTO> getAllEditeur(){
+        return editeurRepo.findAll()
+                .stream().map(editeurMapper::toDTO)
+                .toList();
+
+    }
+
+    public EditeureDTO getEditeur(Long id){
+        Editeure editeure = editeurRepo.findById(id).orElseThrow(()->new RuntimeException("Editeure introuvable"));
+
+        return editeurMapper.toDTO(editeure);
+    }
+
+    public  String deleteEditeur(Long id ){
+        Editeure editeure = editeurRepo.findById(id).orElseThrow(()->new RuntimeException("Editeur introuvable"));
+
+        editeurRepo.deleteById(id);
+
+        return "Editeur   " + editeure.getNom() + " supprimé avec succès.";
+    }
+
+    public  EditeureDTO updateEditeur(Long id,EditeureDTO dto){
+        Editeure editeure = editeurRepo.findById(id).orElseThrow(()-> new RuntimeException("Editeur introuvable"));
+
+        editeure.setNom(dto.getNom());
+
+        Editeure saved = editeurRepo.save(editeure);
+
+        return editeurMapper.toDTO(saved);
+    }
+
+
 }
